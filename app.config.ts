5d79989fe5521f3eb425e847590bf027cfb83ed0
we-khushal-y001 <132071535+we-khushal-y001@users.noreply.github.com>
2023-05-31 15:35:17 +0530

    Initial commit

---
+++ app.config.ts
@@ -0,0 +1,27 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Flutter',
+    description: 'We will guide you',
+    image:
+      'https://docs.flutter.dev/assets/images/shared/brand/flutter/logo/flutter-lockup.png',
+    socials: {
+      twitter: 'nuxtstudio',
+      github: 'nuxt-themes/docus',
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      logo: false,
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'simple-icons:nuxtdotjs',
+        },
+      ],
+    },
+  },
+});
